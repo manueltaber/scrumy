01f81c1c16de5568caa65c899da4e7f6b77e2ffc
Manuel Taber <manuel.taber@topcontrol.local>
2017-03-14 13:14:48 +0100

    Initial commit

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+
+  public formGroup: FormGroup;
+  public developers: number = 4;
+  public velocity: number = 0.8;
+  public days: number = 10;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      developers: [this.developers, Validators.required],
+      velocity: [this.velocity, Validators.required],
+      days: [this.days, Validators.required]
+    });
+
+    this.formGroup.valueChanges.subscribe(data => {
+      console.log('Form changes', data);
+      this.developers = data['developers'];
+      this.velocity = data['velocity'];
+      this.days = data['days'];
+    });
+  }
+
+  public calculateTeamPerformance(): number {
+    return this.developers * this.velocity * this.days;
+  }
+
+  public getFormattedTeamPerformance(): string {
+    return String(this.calculateTeamPerformance()) + ' days';
+  }
+}
